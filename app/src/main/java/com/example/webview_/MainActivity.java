18d37be847a0c98092b644c309cbd4b4a53ec4c1
TaimoorMirza1212 <taimoorhamza199@gmail.com>
2021-07-28 13:57:25 +0500

    Web-View

---
+++ app/src/main/java/com/example/webview_/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.webview_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else {
+        super.onBackPressed();
+    }}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=findViewById(R.id.wb);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com/");
+    }
+}
